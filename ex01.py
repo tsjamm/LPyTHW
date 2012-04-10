4add584d9c4e4c44e4c51fb86e22c242f73b5b7f
Sai Teja <tsjamm@gmail.com>
2012-04-10 11:00:54 +0530

    Exercise1

---
+++ ex01.py
@@ -0,0 +1,6 @@
+print "Hello World!"
+print "Hello Agiliq"
+print "This is neat..."
+print 'Wow! Printing . . .'
+print "I'm now a 'Python' Programmer!"
+print 'I "Eat" Python. I "Walk" Python. I "Think" Python... :P'
