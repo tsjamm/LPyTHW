8cb712a5843ab7a69cfacc2b5143576714b8f2be
Sai Teja <tsjamm@gmail.com>
2012-04-10 11:37:16 +0530

    Exercise 2

---
+++ ex02.py
@@ -0,0 +1,9 @@
+# This is called as a comment.
+# Comments are used to add description of logic.
+# Most useful for later reference.
+
+print "This file is ex02.py" # and this won't print
+
+# Comments are especially useful when testing, to disable a line of code
+#print "disabled line"
+print "enabled line"
