e9e283da4eecdf178b980c8a7565d59bbabaef78
Sai Teja <tsjamm@gmail.com>
2012-04-10 13:01:09 +0530

    Exercise 3 FloatingPoint

---
+++ ex03_1.py
@@ -0,0 +1,24 @@
+print "I will now count my dragons:"
+
+print "Female Dragons", 25+30/6
+print "Male Dragons", 100-25*3%4
+
+print "Now for the Dragon Eggs:"
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3+2<10-7 ?"
+print 3+2<10-7
+
+print "Analyzing...."
+print "3+2=", 3+2
+print "10-7=", 10-7
+
+print "Thats why it's False."
+
+print "Some more practicing: 5 and -2"
+
+print "Is it greater?", 5 > -2
+print "Is is greater or equal?",5 >= -2
+print "Is it less or equal?", 5 <= -2
+
